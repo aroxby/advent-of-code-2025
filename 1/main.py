59dbe16dbe5088b5eb1c5520381878a0d8e7f759
Andy Roxby <aroxby@users.noreply.github.com>
2025-12-03 09:57:29 -0500

    Dumbest possible solution for Day 1, Part 2

--- 1/main.py
+++ 1/main.py
@@ -6,22 +6,32 @@ def main():
     size = 100
     pos = 50
     zeros = 0
+    clicks = True
     for line in sys.stdin:
         line = line.strip()
         if not line:
             continue
         letter, turns = line[0], int(line[1:])
 
-        turns %= size
         if letter == "L":
             turns = -turns
         # else assume R
-        pos += turns
-        if pos < 0:
-            pos += size
-        pos %= size
 
-        if pos == 0:
+        while turns < 0:
+            turns += 1
+            pos -= 1
+            pos %= size
+            if clicks and pos == 0:
+                zeros += 1
+
+        while turns > 0:
+            turns -= 1
+            pos += 1
+            pos %= size
+            if clicks and pos == 0:
+                zeros += 1
+
+        if not clicks and pos == 0:
             zeros += 1
 
     print(zeros)

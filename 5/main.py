513cb59743358a88d60e56f0dcecb58457d1c6b9
Andy Roxby <aroxby@users.noreply.github.com>
2025-12-11 11:30:14 -0500

    Day 5 part 2

--- 5/main.py
+++ 5/main.py
@@ -4,6 +4,7 @@ import sys
 
 def main():
     fresh = 0
+    totalling = True
 
     fresh_ranges = []
     for line in sys.stdin:
@@ -14,19 +15,43 @@ def main():
         else:
             break
 
-    for line in sys.stdin:
-        line = line.strip()
-        if not line:
-            continue
+    if totalling:
+        old_len = -1
+        while len(fresh_ranges) != old_len:
+            print(len(fresh_ranges))
+            old_len = len(fresh_ranges)
+            fresh_ranges = reduce(fresh_ranges)
 
-        iid = int(line)
         for frange in fresh_ranges:
-            # print(iid, frange, iid in frange)
-            if iid in frange:
-                fresh += 1
-                break
+            fresh += len(frange)
+    else:
+        for line in sys.stdin:
+            line = line.strip()
+            if not line:
+                continue
+
+            iid = int(line)
+            for frange in fresh_ranges:
+                # print(iid, frange, iid in frange)
+                if iid in frange:
+                    fresh += 1
+                    break
 
     print(fresh)
+
+
+def reduce(fresh_ranges):
+    new_ranges = []
+    for frange in fresh_ranges:
+        any_overlap = False
+        for idx, nrange in enumerate(new_ranges):
+            overlap = bool(range(max(frange[0], nrange[0]), min(frange[-1], nrange[-1]) + 1))
+            if overlap:
+                new_ranges[idx] = range(min(frange.start, nrange.start), max(frange.stop, nrange.stop))
+            any_overlap = any_overlap or overlap
+        if not any_overlap:
+            new_ranges.append(frange)
+    return new_ranges
 
 
 if __name__ == "__main__":

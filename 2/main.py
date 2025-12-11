ea36fc7061bee642d344e40ed9b0c1babf53dca3
Andy Roxby <aroxby@users.noreply.github.com>
2025-12-10 20:32:30 -0500

    Day 2 part 2

--- 2/main.py
+++ 2/main.py
@@ -5,18 +5,29 @@ import sys
 def main():
     ranges = sys.stdin.read().split(",")
     sum = 0
-    multiples = False
+    multiples = True
+    used = set()
     for r in ranges:
         lower, upper = (int(s) for s in r.split("-"))
+        seen = set()
         for pid in range(lower, upper + 1):
-            spid = str(pid)
-            if len(spid) % 2 != 0:
+            if pid in seen:
                 continue
-            mids = range(len(spid) // 2 + 1) if multiples else [len(spid) // 2]
-            for mid in mids:
-                left, right = spid[:mid], spid[mid:]
-                if left + right == right + left:
-                    print(pid)
+            seen.add(pid)
+            spid = str(pid)
+            seq_lens = range(1, len(spid) // 2 + 1) if multiples else [len(spid) // 2]
+            for seq_len in seq_lens:
+                if seq_len == 1 and len(spid) % 2 != 0 and not multiples:
+                    continue
+                if len(spid) % seq_len != 0:
+                    continue
+                prefix = spid[:seq_len]
+                multipler = len(spid) // seq_len
+                new_pid = prefix * multipler
+
+                if new_pid == spid and spid not in used:
+                    print(seq_len, len(spid), pid)
+                    used.add(spid)
                     sum += pid
 
     print(sum)
